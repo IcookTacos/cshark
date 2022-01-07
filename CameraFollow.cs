1344ec38cf6df46721fb9cd222e64bad3731c881
IcookTacos <zeidlitzerik@gmail.com>
2022-01-08 00:25:28 +0100

    CameraFollow script

---
+++ CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject player;
+    public Vector3 offset;
+
+    void Start(){
+        offset = new Vector3(0,2.5f,-8);
+    }
+
+    void Update(){
+
+        this.transform.position = player.transform.position + offset;
+        this.transform.LookAt(player.transform);
+
+    }
+}
